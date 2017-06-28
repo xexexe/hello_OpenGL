0ce35351b9d55b10ac1b6bdf6a9840b6ac24dac7
Mark Ran <ranjun@wondershare.com>
2017-06-28 14:48:03 +0800

    add hello world OpenGL begin.

--- iOS/HelloWorld/HelloWorld/ViewController.m
+++ iOS/HelloWorld/HelloWorld/ViewController.m
@@ -7,8 +7,15 @@
 //
 
 #import "ViewController.h"
+#import <OpenGLES/ES3/gl.h>
+#import <OpenGLES/ES3/glext.h>
 
-@interface ViewController ()
+@interface ViewController ()<GLKViewDelegate>
+{
+    //EAGLContent是苹果在ios平台下实现的opengles渲染层，
+    //用于渲染结果在目标surface上的更新。
+    EAGLContext *context; 
+}
 
 @end
 
@@ -17,13 +24,38 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    context = [[EAGLContext alloc] initWithAPI:kEAGLRenderingAPIOpenGLES3];//这里用的是opengles3.
+    
+    if (!context) {
+        NSLog(@"Failed to create ES context");
+    }
+    
+    GLKView *view = (GLKView *)self.view;
+    view.context = context;
+    view.drawableDepthFormat = GLKViewDrawableDepthFormat24;
+    [EAGLContext setCurrentContext:context];
+    
+    //开启深度测试，就是让离你近的物体可以遮挡离你远的物体。
+    glEnable(GL_DEPTH_TEST);
+    
+    //设置surface的清除颜色，也就是渲染到屏幕上的背景色。
+    glClearColor(0.1, 0.2, 0.3, 1);
 }
 
+- (BOOL)prefersStatusBarHidden {
+    return YES;
+}
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
 
+#pragma mark - delegate
+
+-(void)glkView:(GLKView *)view drawInRect:(CGRect)rect
+{
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);  //清除surface内容，恢复至初始状态。
+}
 
 @end
